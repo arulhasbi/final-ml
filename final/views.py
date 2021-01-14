d258624441e56833a6b1205d7e3b04537ee309ad
arul hasbi <arulhasbi@aruls-MacBook-Pro.local>
2021-01-14 20:43:08 +0800

    update home

--- final/views.py
+++ final/views.py
@@ -43,7 +43,7 @@ def generateStudent():
     df["PROFILE_CODE"] = df['PROFILE'].apply(profileCoding)
 
     columns = df[['USER_ID', 'NAME', 'AVG_SCORE_DATASCIENCE', 'AVG_SCORE_BACKEND',	'AVG_SCORE_FRONTEND']].columns
-    chunk = df[['USER_ID', 'NAME', 'AVG_SCORE_DATASCIENCE', 'AVG_SCORE_BACKEND',	'AVG_SCORE_FRONTEND', 'PROFILE', 'PROFILE_CODE']].head(10).to_dict('records')
+    chunk = df[['USER_ID', 'NAME', 'AVG_SCORE_DATASCIENCE', 'AVG_SCORE_BACKEND',	'AVG_SCORE_FRONTEND', 'PROFILE', 'PROFILE_CODE']].head(20).to_dict('records')
 
     payload = {
         "columns": columns,
