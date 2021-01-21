435f9a106491f755009ec82c6d3eaee479a5a9ad
arul hasbi <arulhasbi@aruls-MacBook-Pro.local>
2021-01-21 21:04:04 +0800

    customize courses

--- final/views.py
+++ final/views.py
@@ -42,26 +42,53 @@ def home(request):
 
 # make a recommendation page
 def recommendation(request):
-    courses = generateCourse()
+    param = request.GET.get('label', '')
+    courses = generateCourse(param)
     return render(request, 'recommendation.html', {
         'courses': courses
     })
 
 # generate courses data
-def generateCourse():
-    courses = [{
-        "image": "https://images.unsplash.com/photo-1501526029524-a8ea952b15be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-        "title": "MACHINE LEARNING: AN INTRODUCTION",
-        "lecturer": "Prof. Dr. Shaliza K",
-        "hours": 5
-    },
-    {
-        "image": "https://images.unsplash.com/photo-1496942299866-9e7ab403e614?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-        "title": "ARTIFICIAL INTELLIGENCE: AN INTRODUCTION",
-        "lecturer": "Dr. Nilam N",
-        "hours": 4
-    }]
-    return courses
+def generateCourse(param):
+    courses = {
+        5: [{
+                "image": "https://images.unsplash.com/photo-1501526029524-a8ea952b15be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+                "title": "INTRO TO JAVASCRIPT",
+                "lecturer": "Prof. Dr. Shaliza K",
+                "hours": 5
+            }],
+        2: [{
+                "image": "https://images.unsplash.com/photo-1501526029524-a8ea952b15be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+                "title": "INTRO TO ANGULARJS",
+                "lecturer": "Prof. Dr. Shaliza K",
+                "hours": 5
+            }],
+        4: [{
+                "image": "https://images.unsplash.com/photo-1501526029524-a8ea952b15be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+                "title": "MACHINE LEARNING: AN INTRODUCTION",
+                "lecturer": "Prof. Dr. Shaliza K",
+                "hours": 5
+            }],
+        3: [{
+                "image": "https://images.unsplash.com/photo-1501526029524-a8ea952b15be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+                "title": "PHP BASIC",
+                "lecturer": "Dr. Syahid",
+                "hours": 5
+            }],
+        1: [{
+                "image": "https://images.unsplash.com/photo-1496942299866-9e7ab403e614?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+                "title": "ARTIFICIAL INTELLIGENCE: AN INTRODUCTION",
+                "lecturer": "Dr. Nilam N",
+                "hours": 4
+            }],
+        0: [{
+                "image": "https://images.unsplash.com/photo-1501526029524-a8ea952b15be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+                "title": "DEPLOYMENT: INTRO TO DJANGO",
+                "lecturer": "Dr. Syahid",
+                "hours": 5
+            }]
+    }
+    return courses[int(param)]
 
 # generate students data
 def generateStudent():
@@ -121,7 +148,7 @@ def generateRecommendation():
     df["PROFILE"] = preds
     # chunk the dataset
     columns = df[['USER_ID', 'NAME', 'AVG_SCORE_DATASCIENCE', 'AVG_SCORE_BACKEND',	'AVG_SCORE_FRONTEND', 'PROFILE']].columns
-    chunk = df[['USER_ID', 'NAME', 'AVG_SCORE_DATASCIENCE', 'AVG_SCORE_BACKEND', 'AVG_SCORE_FRONTEND', 'PROFILE', 'PROFILE_CODE']].head(50).to_dict('records')
+    chunk = df[['USER_ID', 'NAME', 'AVG_SCORE_DATASCIENCE', 'AVG_SCORE_BACKEND', 'AVG_SCORE_FRONTEND', 'PROFILE', 'PROFILE_CODE']].to_dict('records')
     # encode students' average score for every subject
     datascience = json.dumps(df['AVG_SCORE_DATASCIENCE'].tolist())
     backend = json.dumps(df['AVG_SCORE_BACKEND'].tolist())
